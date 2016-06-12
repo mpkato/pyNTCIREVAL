3e2a148b81ce0e14b8db744dbb9f74e6960f55f6
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-06-13 00:33:56 +0900

    Change load path of main

--- tests/scripts_test.py
+++ tests/scripts_test.py
@@ -2,7 +2,7 @@
 import unittest
 import nose
 from click.testing import CliRunner
-from pyNTCIREVAL.scripts.main import cli
+from pyNTCIREVAL.main import cli
 
 class ScriptsTestCase(unittest.TestCase):
     def setUp(self):
@@ -10,5 +10,5 @@ class ScriptsTestCase(unittest.TestCase):
 
     def test_say(self):
         runner = CliRunner()
-        result = runner.invoke(cli, ['say'])
+        result = runner.invoke(cli, ['label'])
         self.assertEqual(result.output, 'Hello World\n')
