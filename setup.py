a012fe2e6306cb1043daf320da981be7966e91e1
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-09-06 12:29:38 +0900

    Add package pyNTCIREVAL.metrics

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "pyNTCIREVAL",
-    packages = ["pyNTCIREVAL"],
+    packages = ["pyNTCIREVAL", "pyNTCIREVAL.metrics"],
     version = "0.0.1",
     description = "python version of NTCIREVAL",
     author = "Makoto P. Kato",
