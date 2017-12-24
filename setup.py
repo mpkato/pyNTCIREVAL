4c52a526e2dca453c1e4d40021edb26df57d14d1
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2017-12-24 23:00:49 +0900

    Update the version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name = "pyNTCIREVAL",
     packages = ["pyNTCIREVAL", "pyNTCIREVAL.metrics"],
-    version = "0.0.1",
-    description = "python version of NTCIREVAL",
+    version = "0.0.2",
+    description = "Python version of NTCIREVAL",
     author = "Makoto P. Kato",
     author_email = "kato@dl.kuis.kyoto-u.ac.jp",
     license     = "MIT License",
