6104d1a1f9a6b65c5befde8dac01cfa39c0c3bfe
tagaihahimazin <tagainao@icloud.com>
2023-07-31 23:35:43 +0900

    FIX: Recall Program

--- pyNTCIREVAL/metrics/recall.py
+++ pyNTCIREVAL/metrics/recall.py
@@ -1,11 +1,11 @@
 from .metric import Metric
 
 class Recall(Metric):
-    def __init__(self, total_positive):
-        self.total_positives = total_positive
+    def __init__(self, xrelnum):
+        self.total_positives = sum(xrelnum[1:])
 
     def compute(self, labeled_ranked_list):
-        true_positives = sum(item[1] if item[1] is not None else 0 for item in labeled_ranked_list)
+        true_positives = sum(grade for docid, grade in labeled_ranked_list if grade is not None and grade > 0)
         if true_positives == 0:
             return 0.0
         return true_positives / self.total_positives
--- tests/test_recall.py
+++ tests/test_recall.py
@@ -8,15 +8,15 @@ class TestRecall(object):
         from pyNTCIREVAL.metrics import Recall
         
         qrels = {0: 1, 1: 0, 2: 0, 3: 0, 4: 1, 5: 0, 6: 0, 7: 1, 8: 0, 9: 0}
-        ranked_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] # a list of document IDs
+        ranked_list = [0, 1, 3, 5, 8, 9] # a list of document IDs
 
         # labeling: [doc_id] -> [(doc_id, rel_level)]
         labeler = Labeler(qrels)
         labeled_ranked_list = labeler.label(ranked_list)
         
-        total_positive = len([{docid, grade} for docid, grade in qrels.items() if grade == 1])
-        metric = Recall(total_positive)
-        assert total_positive == 3
+        xrelnum = labeler.compute_per_level_doc_num(2)
+        metric = Recall(xrelnum)
+        assert xrelnum == [7, 3]
         
         result = metric.compute(labeled_ranked_list)
-        assert result == 1.0
+        assert result == 0.3333333333333333
