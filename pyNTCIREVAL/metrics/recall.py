4efc918f2cfb3b013e790e862ef006dae8786d07
tagaihahimazin <tagainao@icloud.com>
2023-09-11 21:21:17 +0900

    ADD: Comments to Recall.py

--- pyNTCIREVAL/metrics/recall.py
+++ pyNTCIREVAL/metrics/recall.py
@@ -1,6 +1,13 @@
 from .metric import Metric
 
 class Recall(Metric):
+    '''
+    Recall
+
+    Args:
+        xrelnum: the number of judged X-rel docs (including 0-rel=judged nonrel).
+        labeled_ranked_list: a list of tuples, where each tuple contains a document ID and its corresponding relevance score.
+    '''
     def __init__(self, xrelnum):
         self.total_positives = sum(xrelnum[1:])
 
